17c0a7737575f60938b7fbf918357b2d890ae5fe
Raimis <rggrabauskas@gmail.com>
2021-12-30 12:46:23 +0200

    added header, npm i antd

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import { Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import Header from './component/nav/Header';
 import Login from './pages/auth/Login';
 import Register from './pages/auth/Register';
 import Home from './pages/Home';
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" exact element={<Home />} />
-      <Route path="/login" exact element={<Login />} />
-      <Route path="/register" exact element={<Register />} />
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/login" exact element={<Login />} />
+        <Route path="/register" exact element={<Register />} />
+      </Routes>
+    </>
   );
 };
 
