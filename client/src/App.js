1d89942d92b5e161db053945f9545899fafeae29
Raimis <rggrabauskas@gmail.com>
2021-12-30 10:59:40 +0200

    npm i react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Router, Route, Routes } from 'react-router-dom';
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
+import Home from './pages/Home';
 
 const App = () => {
   return (
-    <div className="App">
-      <p>react app</p>
-    </div>
+    <Routes>
+      <Route path="/" exact element={<Home />} />
+      <Route path="/login" exact element={<Login />} />
+      <Route path="/register" exact element={<Register />} />
+    </Routes>
   );
 };
 
