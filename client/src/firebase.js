e23bb8870b387ab32aca413758fed38f7f3a687d
Raimis <rggrabauskas@gmail.com>
2021-12-30 14:29:13 +0200

    npm i firebase, added firebase.js

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import * as firebase from 'firebase';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDpKozUGsCbsflXtxdm0QS0RY-l0_vb3CU',
+  authDomain: 'ecommerce-9381b.firebaseapp.com',
+  projectId: 'ecommerce-9381b',
+  storageBucket: 'ecommerce-9381b.appspot.com',
+  messagingSenderId: '609738377403',
+  appId: '1:609738377403:web:b2fb608c61c6251dc0019e',
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
