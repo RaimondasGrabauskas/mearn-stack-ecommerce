924c671a91a5f849832eaa9ac54556a933617d6a
Raimis <rggrabauskas@gmail.com>
2022-01-27 14:53:38 +0200

    categorys model schema created

---
+++ server/models/category.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const categorySchema = new Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: 'Name is required',
+      minlength: [3, 'To short'],
+      maxlength: [32, 'Too long'],
+    },
+    slug: {
+      type: String,
+      unique: true,
+      lowercase: true,
+      index: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Category', categorySchema);
