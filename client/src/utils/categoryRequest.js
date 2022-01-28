119926fd9781738c46631732ceb4c45966c74e01
Raimis <rggrabauskas@gmail.com>
2022-01-28 09:40:14 +0200

    category request added

---
+++ client/src/utils/categoryRequest.js
@@ -0,0 +1,36 @@
+import { async } from '@firebase/util';
+import axios from 'axios';
+
+// url
+
+const url = 'http://localhost:8000';
+const createCategoryUrl = url + '/category';
+const getAllCategoriesUrl = url + '/categories';
+const getOneCategoryUrl = url + '/category/';
+const updateCategoryUrl = url + '/update/category/';
+const deleteCategoryUrl = url + '/delete/category/';
+
+export const getCategories = async () => await axios.get(getAllCategoriesUrl);
+
+export const getSingleCategory = async (slug) => await axios.get(getOneCategoryUrl + slug);
+
+export const deleteCategory = async (slug, authtoken) =>
+  await axios.delete(deleteCategoryUrl + slug, {
+    headers: {
+      authtoken,
+    },
+  });
+
+export const updateCategory = async (slug, categoryDetails, authtoken) =>
+  await axios.put(updateCategoryUrl + slug, categoryDetails, {
+    headers: {
+      authtoken,
+    },
+  });
+
+export const createCategory = async (categoryDetails, authtoken) =>
+  await axios.post(createCategoryUrl, categoryDetails, {
+    headers: {
+      authtoken,
+    },
+  });
