5673bd083d460b6254c182b4868cce1b9d51af2f
Raimis <rggrabauskas@gmail.com>
2022-01-28 10:02:49 +0200

    dynamic dashboard

--- client/src/component/nav/Header.js
+++ client/src/component/nav/Header.js
@@ -40,8 +40,16 @@ const Header = () => {
           title={user.email && user.email.split('@')[0]}
           className="ml-auto"
         >
-          <Item key="setting:1">Option 1</Item>
-          <Item key="setting:2">Option 2</Item>
+          {user && user.role === 'subscriber' && (
+            <Item key="setting:1">
+              <Link to="/user/history">Dashboard</Link>
+            </Item>
+          )}
+          {user && user.role === 'admin' && (
+            <Item key="setting:1">
+              <Link to="/admin/dashboard">Dashboard</Link>
+            </Item>
+          )}
           <Item key="logout" icon={<LogoutOutlined />} onClick={logout}>
             Logout
           </Item>
