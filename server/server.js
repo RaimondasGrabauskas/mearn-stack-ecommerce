fe52639f6fbfeb8b0233bce09c787c0d4a665e7e
Raimis <rggrabauskas@gmail.com>
2022-02-23 09:43:32 +0200

    change limits, and cors policy

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
-const bodyParser = require('body-parser');
 const cors = require('cors');
 const fs = require('fs');
 
@@ -21,8 +20,16 @@ mongoose
   .catch((err) => console.error(err.message));
 
 // middlewares
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+  next();
+});
+
 app.use(morgan('dev'));
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb' }));
 app.use(cors());
 
 app.get('/', (req, res) => {
