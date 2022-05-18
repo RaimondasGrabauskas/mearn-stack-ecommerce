0703f57517bc9222014aa00dcd881dd41c5fec29
Raimis <rggrabauskas@gmail.com>
2022-05-18 10:31:16 +0300

    best sellers carousel

--- client/src/component/home/BestSellers.js
+++ client/src/component/home/BestSellers.js
@@ -2,19 +2,26 @@ import { useEffect, useState } from 'react';
 
 import LoadingCard from '../cards/LoadingCard';
 import ProductCard from '../cards/ProductCard';
-import { getProducts } from '../../utils/productRequest';
+import { getProducts, getProductsCount } from '../../utils/productRequest';
+import { Pagination } from 'antd';
 
 const BestSellers = () => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [productsCount, setProductsCount] = useState(0);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
     loadAllProducts();
+  }, [page]);
+
+  useEffect(() => {
+    getProductsCount().then((res) => setProductsCount(res.data));
   }, []);
 
   const loadAllProducts = () => {
     setLoading(true);
-    getProducts('sold', 'desc', 3)
+    getProducts('sold', 'desc', page)
       .then((res) => {
         setProducts(res.data);
         setLoading(false);
@@ -26,18 +33,26 @@ const BestSellers = () => {
   };
 
   return (
-    <div className="container">
-      {loading && <LoadingCard count={3} />}
-      {!loading && (
-        <div className="row">
-          {products.map((p) => (
-            <div className="col-md-4" key={p._id}>
-              <ProductCard product={p} />
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
+    <>
+      <div className="container">
+        {loading && <LoadingCard count={3} />}
+        {!loading && (
+          <div className="row">
+            {products.map((p) => (
+              <div className="col-md-4" key={p._id}>
+                <ProductCard product={p} />
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+
+      <div className="row">
+        <nav className="col-md-4 offset-md-4 text-center pt-5 p-3">
+          <Pagination current={page} total={Math.round(productsCount / 3) * 10} onChange={(value) => setPage(value)} />
+        </nav>
+      </div>
+    </>
   );
 };
 
